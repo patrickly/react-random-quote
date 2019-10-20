eef7149ca4ca7c1801330faf5bb187a47b737693
Patrick <patrickkimly@gmail.com>
2019-10-20 15:22:56 -0700

    Logic 1: Pick a random quote from API

--- src/App.js
+++ src/App.js
@@ -24,13 +24,44 @@ class App extends Component {
     let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 
     axios.get(url)
-       .then(res => console.log(res))
+    .then(res => {
+       let data = res.data.quotes
+       let quoteNum = Math.floor(Math.random() * data.length) //quote number
+       let randomQuote = data[quoteNum] //actual quote
+
+      console.log('randomQuote: ', randomQuote);
+       this.setState({
+          // quote: randomQuote['quote'],
+          quote: randomQuote.quote,
+          author: randomQuote['author']
+       })
+
+       console.log('this.state.quote : ', this.state.quote);
+
+    })
  }
+
+ getNewQuote = () => {
+  this.getQuote()
+}
 
 
   render(){
     console.log('render method runs')
-    return (<div>hzi</div>)
+
+    const { quote, author } = this.state;
+
+    return (
+      <div id="wrapper">
+        <h1 className='title'>Random Quote App</h1>
+        <div id='quote-box'>
+          <div id='text'><p>{quote}</p></div>
+          <div id='author'><h5>{author}</h5></div>
+          {/* <div id='text'><p>{this.state.quote}</p></div>
+          <div id='author'><h5>{this.state.author}</h5></div> */}
+        </div>
+      </div>
+    )
   }
 }
 
