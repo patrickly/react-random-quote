9c42f1341ed840937f9cd25a7d49fab95f965de4
Patrick <patrickkimly@gmail.com>
2019-10-20 14:17:48 -0700

    Added axios and confirmed that data was fetched

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios'
 
 
 class App extends Component {
@@ -8,11 +9,24 @@ class App extends Component {
 
     console.log('constructor runs')
 
+    this.state = {
+      quote: '',
+      author: ''
+   }
   }
 
   componentDidMount() {
     console.log('componentDidMount runs')
+    this.getQuote();
   }
+
+  getQuote() {
+    let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+    axios.get(url)
+       .then(res => console.log(res))
+ }
+
 
   render(){
     console.log('render method runs')
